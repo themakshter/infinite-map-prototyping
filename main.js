a68274ab02a0aac1836750dfef47379f803d66c4
themakshter <mohammad_ali93@live.com>
2018-03-01 19:09:24 +0000

    Multiple changes
    - Rename island
    - Add schema for map object plus example map object
    - Move all other code into separate function to remove it from be made

--- main.js
+++ main.js
@@ -6,6 +6,60 @@ var panelHeight = 600;
 userX = panelWidth/2 - 10;
 userY = panelHeight - 30;
 
+
+
+var mapToLoadSchema = {
+    name: 'string',
+    background: 'svg',
+    overlay: 'svg',
+    activities: [
+        {
+            name: 'string',
+            image: 'svg',
+            x: 'int',
+            y: 'int'
+        }
+    ]
+};
+
+var exampleMapToLoad = {
+    name: 'Fun Island',
+    background: 'water-background.jpg',
+    overlay: 'island.png',
+    activities:[
+        {
+            name: 'Freaky Forest',
+            image: 'forest.png',
+            x: 10,
+            y: 10
+        },
+        {
+            name: 'Mourny Mountain',
+            image: 'mountain.jpg',
+            x: 50,
+            y: 50
+        },
+        {
+            name: 'Raging River',
+            image: 'river.png',
+            x: 250,
+            y: 25
+        },
+        {
+            name: 'Creepy Castle',
+            image: 'castle.svg',
+            x: 500,
+            y: 700
+        },
+        {
+            name: 'X Marks the Spot',
+            image: 'x-mark.png',
+            x: 350,
+            y: 350
+        }
+    ]
+}
+
 class ActivityComponent{
     constructor(name, imageSource, x, y){
         this.name = name;
@@ -33,70 +87,71 @@ class ActivityComponent{
     }
 }
 
+function doSomePrototyping(){
+    var forest = createForest(50,30);
 
-var forest = createForest(50,30);
-
-var user = createUser(userX, userY);
-
-var streamLine = createStream(200, 200);
-
-var pathToForest = createPath(userX, userY, 80, 75);
-
-var castle =  new ActivityComponent("Dream Castle", 'castle.svg', 400, 150);
-castle.drawComponent();
-
-function createPath(startX, startY, finishX, finishY){
-    var pathBetween = "M " + startX + "," + startY + " " + finishX + "," + finishY;
-    return s.path(pathBetween)
-            .attr({stroke:"orange", fill:"transparent", strokeDasharray:10, strokeWidth:5});
-}
-
-function createUser(x, y){
-    return s.image('x-mark.png', x, y, 25, 25);
-}
-
-function createStream(x, y){
-    for(var i = 0; i < 3; i++){
-        var streamLine = createStreamWave(x, y);
-        y += 10;
+    var user = createUser(userX, userY);
+    
+    var streamLine = createStream(200, 200);
+    
+    var pathToForest = createPath(userX, userY, 80, 75);
+    
+    var castle =  new ActivityComponent("Dream Castle", 'castle.svg', 400, 150);
+    castle.drawComponent();
+    
+    function createPath(startX, startY, finishX, finishY){
+        var pathBetween = "M " + startX + "," + startY + " " + finishX + "," + finishY;
+        return s.path(pathBetween)
+                .attr({stroke:"orange", fill:"transparent", strokeDasharray:10, strokeWidth:5});
     }
-}
-
-function createStreamWave(x, y){
-    var pathString = "M " + x + " " + y;
-    var length = 20;
-    for(var i = 0; i < 3; i++){
-        pathString += createStreamCurve(x, y, length);
-        x += length;
+    
+    function createUser(x, y){
+        return s.image('x-mark.png', x, y, 25, 25);
     }
-    return s.path(pathString).attr({ stroke: "blue", fill: "transparent", strokeWidth:5});;
-}
-
-function createStreamCurve(x, y, length){
-    var height = 10;
-    return " Q " + (x + length/2) + " " + (y - height) + " " + (x + length) + " " + y;
-}
-
-
-function createForest(x, y){
-    for(var i = 0; i < 3; i++){
-        createForestLine(x, y, 5)
-        y += 15;
+    
+    function createStream(x, y){
+        for(var i = 0; i < 3; i++){
+            var streamLine = createStreamWave(x, y);
+            y += 10;
+        }
     }
-}
-
-function createForestLine(x, y, numberOfTrees){
-    for(var i = 0; i < numberOfTrees; i++){
-        createTriangleTree(x, y)
-        x += 15;
+    
+    function createStreamWave(x, y){
+        var pathString = "M " + x + " " + y;
+        var length = 20;
+        for(var i = 0; i < 3; i++){
+            pathString += createStreamCurve(x, y, length);
+            x += length;
+        }
+        return s.path(pathString).attr({ stroke: "blue", fill: "transparent", strokeWidth:5});;
     }
-}
-
-function createTriangleTree(x, y){
-    var points = [
-        x,      y, 
-        x + 5,  y - 10,
-        x + 10, y]
-    return s.polygon(points).attr({fill: "green"});
+    
+    function createStreamCurve(x, y, length){
+        var height = 10;
+        return " Q " + (x + length/2) + " " + (y - height) + " " + (x + length) + " " + y;
+    }
+    
+    
+    function createForest(x, y){
+        for(var i = 0; i < 3; i++){
+            createForestLine(x, y, 5)
+            y += 15;
+        }
+    }
+    
+    function createForestLine(x, y, numberOfTrees){
+        for(var i = 0; i < numberOfTrees; i++){
+            createTriangleTree(x, y)
+            x += 15;
+        }
+    }
+    
+    function createTriangleTree(x, y){
+        var points = [
+            x,      y, 
+            x + 5,  y - 10,
+            x + 10, y]
+        return s.polygon(points).attr({fill: "green"});
+    }
 }
 
